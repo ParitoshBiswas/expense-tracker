b7606179d92bd010148864d4c8a96117f442f26f
Paritosh Biswas <53013257+ParitoshBiswas@users.noreply.github.com>
2022-10-10 19:59:23 +0530

    React State and Working with Events

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css';
 
-function Card(props){                          // wrapper component using "children" property
+const Card = (props) => {                          // wrapper component using "children" property
     const classes = 'card ' + props.className;  // blank space after 'card ' to align the components.
 
     return <div className={classes}>{props.children}</div>  // children is a reserved name.
