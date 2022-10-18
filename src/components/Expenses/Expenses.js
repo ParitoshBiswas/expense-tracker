85339656e75472e28074c445f1af79048e7e6f84
Paritosh Biswas <53013257+ParitoshBiswas@users.noreply.github.com>
2022-10-18 20:18:58 +0530

    Added conditions and conditional returns based on selected year.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';  //REACT HOOK(useState)
 
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2022'); //filteredYear -> variable itself(initial value)  setFilteredYear -> updating function(can be called to set new value)
@@ -13,13 +13,17 @@ const Expenses = (props) => {
     }
 
 
+    const filteredExpenses = props.items.filter((expense) => {
+        return (
+            expense.date.getFullYear().toString() === filteredYear
+        )
+      })
+
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map((expense) => (
-                    <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
-                ))}
+                <ExpensesList items={filteredExpenses}/>   
             </Card>
         </div>
     )                                      // component inside component(wrapper component).
