a232ca807eb86192bee1c1e7c8b59d6e9c7e6813
Paritosh Biswas <53013257+ParitoshBiswas@users.noreply.github.com>
2022-10-19 20:04:53 +0530

    Added conditions to show NewExpense form or not

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -32,7 +32,6 @@ const ExpenseForm = (props) => {
         setEnteredDate('');
     }
 
-
     return (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
@@ -50,6 +49,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,32 @@
+import { useState } from 'react';
+
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+    const [isAdding, setIsAdding] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData);      //child-to-parent component communication(NewExpense-to-App.js)  //onAddExpense is a function received from App Component.
+        setIsAdding(false);
+    }
+
+    const addNewExpense = () => {
+        setIsAdding(true);
+    }
+
+    const stopAddingHandler = () => {
+        setIsAdding(false);
     }
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isAdding && <button onClick={addNewExpense}>Add New Expense</button>}
+            {isAdding && <ExpenseForm onCancel={stopAddingHandler} onSaveExpenseData={saveExpenseDataHandler} />}
         </div>
     );
 }
